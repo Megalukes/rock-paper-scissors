da5fdfb566d9d68dc44319198fea2601be25f157
Megalukes <megalukes@gmail.com>
2023-01-04 00:01:41 -0300

    Add UI and style, finish main app

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const buttons = document.querySelectorAll('button')
+let options_array = Array.from(buttons)
+// console.log(buttons)
+
+buttons.forEach ( button => {
+    button.addEventListener('click', () => {
+
+        if (player_points >= 5 || computer_points >= 5){
+            player_points = 0
+            computer_points = 0
+        }
+        let pChoice = button.id
+        let cChoice = getComputerChoice()
+
+        let div1 = document.querySelector('#new-score')
+        div1.textContent = playRound(pChoice, cChoice)
+
+        let div0 = document.querySelector('#current-score')
+        div0.textContent = "Player: " + player_points + " point(s) | " + "Computer: " + computer_points + " point(s)\n"
+
+        if (player_points == 5) {
+            div0.textContent = "PLAYER WINS!"
+            div1.textContent = ""
+        } else if (computer_points == 5) {
+            div0.textContent = "COMPUTER WINS!"
+            div1.textContent = ""
+        }
+
+    })
+})
+
 let round = 1
 let player_points = 0
 let computer_points = 0
@@ -9,7 +40,7 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-    let current = "Player chooses " + String(playerSelection) + "\nComputer chooses " + String(computerSelection) + "\n"
+    let current = "Player chooses " + String(playerSelection) + " and Computer chooses " + String(computerSelection) + ". "
 
     if (playerSelection == computerSelection) {
         return current + "Draw! Try again."
@@ -47,26 +78,26 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     
-    for (let i = 0; i < 5; i++){
+    // for (let i = 0; i < 5; i++){
 
-        let playerSelection = String(prompt("rock, paper or scissors?"))
-        let computerSelection = getComputerChoice()
+    //     let playerSelection = String(prompt("rock, paper or scissors?"))
+    //     let computerSelection = getComputerChoice()
 
-        console.log("Round " + round)
-        console.log(playRound(playerSelection, computerSelection))
+    //     console.log("Round " + round)
+    //     console.log(playRound(playerSelection, computerSelection))
 
-        console.log("Player: " + player_points + " point(s)\n" + "Computer: " + computer_points + " point(s)\n")
+    //     console.log("Player: " + player_points + " point(s)\n" + "Computer: " + computer_points + " point(s)\n")
         
-        if (player_points > computer_points) {
-            console.log("Player wins!")
-        }
-        else if (player_points < computer_points) {
-            console.log("Computer wins!")
-        }
-        else {
-            console.log("Draw!")
-        }
-    }
+    //     if (player_points > computer_points) {
+    //         console.log("Player wins!")
+    //     }
+    //     else if (player_points < computer_points) {
+    //         console.log("Computer wins!")
+    //     }
+    //     else {
+    //         console.log("Draw!")
+    //     }
+    // }
 }
 
 game()
